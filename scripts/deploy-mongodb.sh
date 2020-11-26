c7256dfe97b32938151df1ebf2e5e2ee087502aa
darteaga <darteaga@vmware.com>
2020-11-26 20:02:34 +0000

    feat: add scripts
    Signed-off-by: darteaga <darteaga@vmware.com>

---
+++ scripts/deploy-mongodb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mongodb_release_name=mongodb
+mongodb_namespace=default
+database_name=innosoft-demo
+database_username=innosoft-demo-user
+
+if ! helm status -n "${mongodb_namespace}" "${mongodb_release_name}" > /dev/null; then
+    echo "Install bitnami/mongodb mongodb_release_name=${mongodb_release_name} mongodb_namespace=${mongodb_namespace}"
+
+    helm install --wait mongodb bitnami/mongodb \
+        --set auth.database=${database_name} \
+        --set auth.username=${database_username}
+fi
+echo "A release of  bitnami/mongodb, ${mongodb_release_name}, is running on ${mongodb_namespace} namespace"
+
+echo "Mongodb password:  k get secrets mongodb -o=jsonpath='{.data.mongodb-password}' | base64 --decode"
---
+++ scripts/deploy-monitoring.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+## Installing and configuring prometheus
+prometheus_namespace="monitoring"
+prometheus_release_name="prom"
+prometheus_host="prometheus-operated"
+prometheus_port="9090"
+
+if ! kubectl get namespace "${prometheus_namespace}" > /dev/null; then
+    echo "A namespace called ${prometheus_namespace} for prometheus should exist, creating it"
+    kubectl create namespace "${prometheus_namespace}" > /dev/null
+fi
+echo "A namespace called ${prometheus_namespace} exists in the cluster"
+
+if ! helm status -n "${prometheus_namespace}" "${prometheus_release_name}" > /dev/null; then
+    echo "Install bitnami/kube-prometheus prometheus_release_name=${prometheus_release_name} prometheus_namespace=${prometheus_namespace}"
+
+    helm upgrade --wait -n "${prometheus_namespace}" --install "${prometheus_release_name}" \
+        bitnami/kube-prometheus > /dev/null
+fi
+echo "A release of bitnami/kube-prometheus, ${prometheus_release_name}, is running on ${prometheus_namespace} namespace"
+
+## Installing and configuring grafana
+grafana_namespace="monitoring"
+grafana_release_name="graf"
+
+if ! kubectl get namespace "${grafana_namespace}" > /dev/null; then
+    echo "A namespace called ${grafana_namespace} for grafana should exist, creating it"
+    kubectl create namespace "${grafana_namespace}" > /dev/null
+fi
+echo "A namespace called ${grafana_namespace} exists in the cluster"
+
+download_dasboard() {
+    local -r dashboard=${1:?dashboard url is required}
+    local -r destination=${2:?destination path is required}
+
+    echo "Downloading dashboard form: ${dashboard}"
+    if ! curl -sSL -o "${destination}" "${dashboard}"; then
+        echo "dashboard does not exist"
+    fi
+}
+
+if ! helm status -n "${grafana_namespace}" "${grafana_release_name}" > /dev/null; then
+    echo "Install bitnami/grafana grafana_release_name=${grafana_release_name} grafana_namespace=${grafana_namespace}"
+
+    grafana_tmp_folder="/tmp/graf.$(date +%s)"
+    mkdir -p "${grafana_tmp_folder}"
+
+    grafana_dashboard_root_url="https://raw.githubusercontent.com/spring-cloud/spring-cloud-dataflow/master/src/grafana/prometheus/docker/grafana/dashboards"
+
+    for dashboard in "scdf-applications" "scdf-streams" "scdf-task-batch"
+    do
+        ! download_dasboard "${grafana_dashboard_root_url}/${dashboard}.json" "${grafana_tmp_folder}/${dashboard}.json" exit 1
+
+        kubectl -n "${grafana_namespace}" delete configmap "grafana-dashboards-${dashboard}" || true && \
+        kubectl -n "${grafana_namespace}" create configmap "grafana-dashboards-${dashboard}" --from-file=${dashboard}.json=${grafana_tmp_folder}/${dashboard}.json
+    done
+
+    cat > "${grafana_tmp_folder}/datasources.yaml" <<EOF
+apiVersion: 1
+datasources:
+- name: ScdfPrometheus
+  type: prometheus
+  access: proxy
+  org_id: 1
+  url: http://${prometheus_host}:${prometheus_port}
+  is_default: true
+  version: 5
+  editable: true
+  read_only: false
+EOF
+
+    kubectl -n "${grafana_namespace}" delete secret "grafana-datasources" || true && \
+        kubectl -n "${grafana_namespace}" create secret generic "grafana-datasources" --from-file=datasources.yaml=${grafana_tmp_folder}/datasources.yaml
+
+    helm upgrade --wait -n "${grafana_namespace}" --install "${grafana_release_name}" \
+        bitnami/grafana \
+        --set metrics.enabled=true \
+        --set imageRenderer.enabled=true \
+        --set plugins=grafana-image-renderer \
+        --set dashboardsProvider.enabled=true \
+        --set dashboardsConfigMaps[0].configMapName=grafana-dashboards-scdf-applications \
+        --set dashboardsConfigMaps[0].fileName=scdf-applications.json \
+        --set dashboardsConfigMaps[1].configMapName=grafana-dashboards-scdf-streams \
+        --set dashboardsConfigMaps[1].fileName=scdf-streams.json \
+        --set dashboardsConfigMaps[2].configMapName=grafana-dashboards-scdf-task-batch \
+        --set dashboardsConfigMaps[2].fileName=scdf-task-batch.json \
+        --set datasources.secretName=grafana-datasources > /dev/null
+fi
+echo "A release of bitnami/grafana, ${grafana_release_name}, is running on ${grafana_namespace} namespace"
+
+echo "PROMETHEUS RESOURCES"
+kubectl get -n "${prometheus_namespace}" ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide -l "app.kubernetes.io/instance=${prometheus_release_name}"
+
+echo ""
+
+echo "GRAFANA RESOURCES"
+kubectl get -n "${grafana_namespace}" ingress,pod,svc,job,cronjob,deployment,rs,pv,pvc,secret,ep -o wide -l "app.kubernetes.io/instance=${grafana_release_name}"
